0c8fd6ec59b800062ce985eee6229487b8b683ef
devops_01 <devops_01@gmail.com>
2026-05-18 16:04:01 +0530

    optimusprime-terraform-eks backendtf, backendhcl, provider, variables, devautotfvars, and versions files have been created....

---
+++ envs/dev/variables.tf
@@ -0,0 +1,6 @@
+############################
+# Core
+############################
+variable "aws_region" { type = string }
+variable "project" { type = string }
+variable "environment" { type = string }
