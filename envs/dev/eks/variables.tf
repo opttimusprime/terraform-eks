9e29fa20aeb416835335566e07c3e0def5072278
devops_01 <devops_01@gmail.com>
2026-05-18 22:05:11 +0530

    optimusprime-terraform-eks eks-cluster logic has been modified....

--- envs/dev/eks/variables.tf
+++ envs/dev/eks/variables.tf
@@ -1,17 +1,11 @@
-############################
-# Core
-############################
-variable "aws_region" { type = string }
-variable "project" { type = string }
-variable "environment" { type = string }
-
-############################
-# Network
-############################
-variable "dev_vpc_cidr" { type = string }
-variable "public_subnet_cidrs" {
-  type = list(string)
+variable "aws_region" {
+  type = string
 }
-variable "private_subnet_cidrs" {
-  type = list(string)
+
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type = string
 }
