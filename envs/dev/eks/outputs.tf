d0a3e354ad7810b61ffc79e566e179d0516b7fef
devops_01 <devops_01@gmail.com>
2026-05-19 23:00:08 +0530

    opttimusprime terraform-eks ebs-csi and namespace setup has been created....

--- envs/dev/eks/outputs.tf
+++ envs/dev/eks/outputs.tf
@@ -10,6 +10,10 @@ output "cluster_security_group_id" {
   value = module.eks.cluster_security_group_id
 }
 
-output "node_security_group_id" {
-  value = module.eks.node_security_group_id
+output "oidc_provider_arn" {
+  value = module.eks.oidc_provider_arn
+}
+
+output "oidc_provider" {
+  value = module.eks.oidc_provider
 }
