f4f2ade2161c0413ee0b6bec3bcf5e1b132fda18
devops_01 <devops_01@gmail.com>
2026-05-19 09:24:16 +0530

    optimusprime-terraform-eks eks-cluster access-entries for console_user logic has been modified....

--- envs/dev/eks/main.tf
+++ envs/dev/eks/main.tf
@@ -61,4 +61,37 @@ module "eks" {
   }
 
   tags = local.common_tags
+
+
+  node_security_group_additional_rules = {
+  ingress_self_all = {
+    description = "Node to node all traffic"
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    type        = "ingress"
+    self        = true
+  }
+
+  egress_all = {
+    description      = "Allow all outbound traffic"
+    protocol         = "-1"
+    from_port        = 0
+    to_port          = 0
+    type             = "egress"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+cluster_security_group_additional_rules = {
+  ingress_nodes_ephemeral = {
+    description                = "Node groups to cluster API"
+    protocol                   = "tcp"
+    from_port                  = 1025
+    to_port                    = 65535
+    type                       = "ingress"
+    source_node_security_group = true
+  }
+}
 }
