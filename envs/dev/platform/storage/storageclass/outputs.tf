28767346a5bb191a528b843809625c309c0486df
devops_01 <devops_01@gmail.com>
2026-05-19 17:27:01 +0530

    opttimusprime terraform-eks storage StorageClass setup has been modified....

--- envs/dev/platform/storage/storageclass/outputs.tf
+++ envs/dev/platform/storage/storageclass/outputs.tf
@@ -1,7 +1,3 @@
-output "ebs_csi_role_arn" {
-  value = module.ebs_csi_irsa_role.iam_role_arn
-}
-
 output "storage_class_name" {
   value = kubernetes_storage_class.gp3.metadata[0].name
 }
--- envs/dev/platform/storage/storageclass/provider.tf
+++ envs/dev/platform/storage/storageclass/provider.tf
@@ -2,3 +2,25 @@ provider "aws" {
   region = var.aws_region
 }
 
+provider "kubernetes" {
+
+  host = data.terraform_remote_state.eks.outputs.cluster_endpoint
+
+  cluster_ca_certificate = base64decode(
+    data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data
+  )
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      data.terraform_remote_state.eks.outputs.cluster_name,
+      "--region",
+      var.aws_region
+    ]
+  }
+}
