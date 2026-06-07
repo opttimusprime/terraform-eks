9294982a76b1d27fda41ab565d6843bf34396ff8
devops_01 <devops_01@gmail.com>
2026-06-07 15:36:59 +0530

    alb-controller setup has been created....

--- envs/dev/platform/networking/alb-controller/locals.tf
+++ envs/dev/platform/networking/alb-controller/locals.tf
@@ -1,4 +1,4 @@
 locals {
   cluster_name = data.terraform_remote_state.eks.outputs.cluster_name
-  vpc_id       = data.terraform_remote_state.eks.outputs.vpc_id
+  vpc_id       = data.aws_eks_cluster.eks.vpc_config[0].vpc_id
 }
