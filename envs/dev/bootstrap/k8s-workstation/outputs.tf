d4ac5388c4dfa9de1e8ba001238ce5eea9ec806c
devops_01 <devops_01@gmail.com>
2026-06-20 16:03:15 +0530

    k8s_workstation_security_group_id has been added into outputs.tf....

--- envs/dev/bootstrap/k8s-workstation/outputs.tf
+++ envs/dev/bootstrap/k8s-workstation/outputs.tf
@@ -1,3 +1,7 @@
 output "k8s_workstation_role_arn" {
   value = aws_iam_role.k8s_workstation_role.arn
 }
+
+output "k8s_workstation_security_group_id" {
+  value = aws_security_group.k8s_workstation.id
+}
